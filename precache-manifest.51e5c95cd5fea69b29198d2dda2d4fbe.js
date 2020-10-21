f62a7742820f1b43d7a9e7962f2d8a5dbca6bcb9
Babin Brahmachari <kingsuk.brahmachari@gmail.com>
2020-10-21 18:22:00 +0530

    Updates

---
+++ precache-manifest.51e5c95cd5fea69b29198d2dda2d4fbe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d35d61f60e690074a0bee43dd58eb5d",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "a79506eb9129c7c99dd0",
+    "url": "/monsters-rolodex/static/css/main.d7eed204.chunk.css"
+  },
+  {
+    "revision": "88b685495fee72b0273c",
+    "url": "/monsters-rolodex/static/js/2.2eaf0afe.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/monsters-rolodex/static/js/2.2eaf0afe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a79506eb9129c7c99dd0",
+    "url": "/monsters-rolodex/static/js/main.a5455c07.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  }
+]);
